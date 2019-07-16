c1d72ab80a9e5749451cfc6b6a86767dfa17c960
Arturo Madrigal <arturo.madrigalv@gmail.com>
2019-07-15 23:39:48 -0600

    Updates

---
+++ precache-manifest.235b129a179a7fd086484b9fe61d1c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8950bda2b54eb3889531c927293cd17c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35ad978bf51919b62c5",
+    "url": "/static/css/main.d926f73a.chunk.css"
+  },
+  {
+    "revision": "c5fd5d8329ddd49e7daf",
+    "url": "/static/js/2.651bbf15.chunk.js"
+  },
+  {
+    "revision": "c35ad978bf51919b62c5",
+    "url": "/static/js/main.39b110a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
